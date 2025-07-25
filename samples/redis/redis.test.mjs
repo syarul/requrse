e52f6b252dade89ca5338797ea7218adae937fb0
Shahrul Nizam Selamat <hottincup@gmail.com>
2025-07-25 20:11:12 +0800

    update redis address

--- samples/redis/redis.test.mjs
+++ samples/redis/redis.test.mjs
@@ -6,8 +6,9 @@ import { test } from '../../test/fixture/test.mjs'
 
 dotenv.config()
 
-const redis = new Redis(`redis://default:${process.env.REDIS_KEY}@redis-14348.c292.ap-southeast-1-1.ec2.redns.redis-cloud.com:14348`)
+const redis = new Redis(`redis://default:${process.env.REDIS_KEY}@${process.env.REDIS_URL}`)
 // const redis = new Redis(`redis://127.0.0.1:6379`)
+
 
 const redisKey = 'books'
 const memberKey = 'books_ids'
